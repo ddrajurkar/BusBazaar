0b409dc3acd1726ac22e9a761fd5ac944bdc2358
ddrajurkar <ddrajurkar@gmail.com>
2018-06-19 11:26:52 +0530

    cmt
    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes,RouterModule } from '@angular/router';
 
+import {SocialLoginModule,AuthServiceConfig,FacebookLoginProvider,GoogleLoginProvider} from "angular5-social-login";
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -11,6 +12,7 @@ import { ContactComponent } from './contact/contact.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { LoginComponent } from './login/login.component';
 import { LoadloginService } from './loadlogin.service';
+import { MyProfileComponent } from './my-profile/my-profile.component';
 
 const appRoutes: Routes = [
   { path: '',
@@ -20,8 +22,28 @@ const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'contactus', component: ContactComponent },
   {path:'login',component:LoginComponent},
-  { path: '**', component: PageNotFoundComponent }
+  { path: '**', component: PageNotFoundComponent },
+  {path:'myProfile',component:MyProfileComponent}
 ];
+
+export function getAuthServiceConfigs()
+{
+let config = new AuthServiceConfig([
+  // {
+  //   id:FacebookLoginProvider.PROVIDER_ID,
+  //   provider:new FacebookLoginProvider("") 
+  // },
+   {
+id:GoogleLoginProvider.PROVIDER_ID,
+provider:new GoogleLoginProvider("468748689868-i1hg3iuq4q3bl5ji1l4oief7fatdooar.apps.googleusercontent.com")
+  },
+  // {
+  //   id:FacebookLoginProvider.PROVIDER_ID,
+  //   provider:new FacebookLoginProvider("")
+  // }
+]);
+return config;
+}
 
 @NgModule({
   declarations: [
@@ -31,16 +53,18 @@ const appRoutes: Routes = [
     FooterComponent,
     ContactComponent,
     PageNotFoundComponent,
-    LoginComponent
+    LoginComponent,
+    MyProfileComponent
   ],
   imports: [
     BrowserModule,
 RouterModule.forRoot(
   appRoutes
   // { enableTracing: true } // <-- debugging purposes only
-    )  
+    ),
+    SocialLoginModule  
 ],
-  providers: [LoadloginService],
+  providers: [LoadloginService,{provide:AuthServiceConfig,useFactory:getAuthServiceConfigs}],
   bootstrap: [AppComponent],
   entryComponents: [LoginComponent]
 })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService,GoogleLoginProvider,FacebookLoginProvider} from "angular5-social-login";
+import { Router } from '@angular/router';
+import { MyProfileComponent } from '../my-profile/my-profile.component';
+import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
 
 @Component({
   selector: 'app-login',
@@ -6,13 +10,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  _ref:any;   
-  removeObject(){
-    this._ref.destroy();
-  }  
-  constructor() { }
+//  _ref:any;   
+  // removeObject(){
+  //   this._ref.destroy();
+  // }  
 
-  ngOnInit() {
+authorized : boolean
+router:Router
+
+  constructor(private SocAuthSrv:AuthService) {
+
+  }
+  
+  public SocialSignIn(socialplatform:string){
+       let socialplatformprovider
+       if(socialplatform == "Google"){
+         socialplatformprovider=GoogleLoginProvider.PROVIDER_ID;
+       }
+       else if(socialplatform == "Facebook"){
+         socialplatformprovider=FacebookLoginProvider.PROVIDER_ID;
+       }    
+       
+       this.SocAuthSrv.signIn(socialplatformprovider).then(
+        (userData) => {
+          //console.log(socialplatform+" sign in data : " , userData);
+          // Now sign-in with userData        
+          if (userData != null) {
+                 this.authorized = true;
+                // this.user = userData;               
+            }});                                     
+  }
+  public gglLogin()
+  {
+    this.SocialSignIn('Google');
+    if(this.authorized)
+    {
+      this.router.navigate([MyProfileComponent]);
+       //this.router.navigateByUrl('myProfile')
+    }
+    else
+    {
+      this.router.navigate([PageNotFoundComponent]);
+      //this.router.navigateByUrl('**')
+    }
   }
 
+  ngOnInit() {
+ 
+  }
 }
